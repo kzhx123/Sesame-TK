64c6bde74b32cb12361279de29c70d2e73b9a766
kzhx123 <orlovanadezda850@gmail.com>
2025-11-25 22:59:00 +0800

    Update AntOrchard.java

--- app/src/main/java/fansirsqi/xposed/sesame/task/antOrchard/AntOrchard.java
+++ app/src/main/java/fansirsqi/xposed/sesame/task/antOrchard/AntOrchard.java
@@ -96,7 +96,7 @@ public class AntOrchard extends ModelTask {
               triggerTbTask();
             }
             Integer orchardSpreadManureCountValue = orchardSpreadManureCount.getValue();
-            if (orchardSpreadManureCountValue > 0 && Status.canSpreadManureToday(userId)) orchardSpreadManure();
+            if (orchardSpreadManureCountValue > 0 orchardSpreadManure();
             if (orchardSpreadManureCountValue >= 3 && orchardSpreadManureCountValue < 10) {
               querySubplotsActivity(3);
             } else if (orchardSpreadManureCountValue >= 10) {
@@ -181,7 +181,8 @@ public class AntOrchard extends ModelTask {
           int happyPoint = Integer.parseInt(accountInfo.getString("happyPoint"));
           int wateringCost = accountInfo.getInt("wateringCost");
           int wateringLeftTimes = accountInfo.getInt("wateringLeftTimes");
-          if (happyPoint > wateringCost && wateringLeftTimes > 0 && (200 - wateringLeftTimes < orchardSpreadManureCount.getValue())) {
+          Log.record(TAG, "DEBUG: 现有肥料=" + happyPoint + ", 单次消耗=" + wateringCost + ", 剩余次数=" + wateringLeftTimes);
+          if (happyPoint > wateringCost && wateringLeftTimes > 0 {
             jo = new JSONObject(AntOrchardRpcCall.orchardSpreadManure(getWua()));
             if (!"100".equals(jo.getString("resultCode"))) {
               Log.record(jo.getString("resultDesc"));
